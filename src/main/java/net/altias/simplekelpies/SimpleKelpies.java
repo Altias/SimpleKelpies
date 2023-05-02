ab3dbdb244c2ee8531d34a7a2eb93d6948396f50
Altias <jzebrablanket@gmail.com>
2023-05-01 21:58:01 -0400

    Initial commit

---
+++ src/main/java/net/altias/simplekelpies/SimpleKelpies.java
@@ -0,0 +1,20 @@
+package net.altias.simplekelpies;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleKelpies implements ModInitializer {
+    public static final String MOD_ID = "simplekelpies";
+    public static final Logger LOGGER = LoggerFactory.getLogger("simplekelpies");
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!");
+    }
+}
---
+++ src/main/java/net/altias/simplekelpies/SimpleKelpiesClient.java
@@ -0,0 +1,11 @@
+package net.altias.simplekelpies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleKelpiesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
